d40441ae8aeabcdef2aa6591f8d8a5cdac82156e
TomasFerreira66 <tomasff2002@gmail.com>
2025-02-10 23:39:05 +0000

    update

--- tomas-hub/src/app/page.tsx
+++ tomas-hub/src/app/page.tsx
@@ -16,13 +16,14 @@ export default function Home() {
   <div className="w-1/2 flex flex-col h-fit m-4 items-center justify-center mx-auto gap-6">
     
     
-    <Link href="/hub/3dmodelviewer" className="group ">
+    <Link href="/hub/3dmodelviewer" className="group w-full">
       <div className="cursor-pointer p-6 bg-white shadow-lg rounded-2xl hover:shadow-xl transition-transform transform group-hover:scale-105 text-center">
         <h2 className="text-xl font-semibold text-gray-800">Go to Second Page</h2>
         <p className="text-gray-600 mt-2">Click this card to navigate.</p>
       </div>
     </Link>
   
+    
 
     
     
